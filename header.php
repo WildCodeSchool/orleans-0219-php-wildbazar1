9965a3e8fb036130a95e12c46c5a3fec269c23f0
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2019-03-14 17:51:46 +0100

    html vers php

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
           crossorigin="anonymous">
     <!-- Appel css -->
-    <link rel="stylesheet" href="css/developers.css">
+    <link rel="stylesheet" href="css/<?php echo $specialCss; ?>.css">
     <title>Wild Bazar</title>
 
 
