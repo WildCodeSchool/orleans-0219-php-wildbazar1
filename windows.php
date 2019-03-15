c930a0a16b1979a439c59dd4c021d5db85e7f64e
lamoureux-sylvain <lamoureux.sylvain@gmail.com>
2019-03-15 08:54:07 +0100

    correction head

--- windows.php
+++ windows.php
@@ -1,9 +1,35 @@
 <?php
-$specialCss = 'style_windows';
+
 $pageTitle = 'Windows';
 $pageUnderTitle = 'let you see the perfect blue screen';
 include 'header.php';
 ?>
+
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,
+            shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+          crossorigin="anonymous">
+
+
+    <link rel="stylesheet" href="../css/style_windows.css">
+
+    <title>Wild Bazar</title>
+</head>
+
+<body>
 
 <main>
 
