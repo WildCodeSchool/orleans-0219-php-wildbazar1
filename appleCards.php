c6225b13755f3dd2cf6e29ffd6852335d6f26a50
maiwenn99 <maiwenngay99@gmail.com>
2019-03-25 21:40:47 +0100

    linked to database

--- appleCards.php
+++ appleCards.php
@@ -1,8 +1,7 @@
-
-
 <div class="card">
-    <div data-toggle="modal" data-target="#<?php echo $key; ?>">
-        <img class="card-img-top img-fluid mt-3" src="images/apple/<?php echo $value[3]; ?>" alt="image du <?php echo $key; ?>">
+    <div data-toggle="modal" data-target="#<?php echo "produit" . ($key + 1); ?>">
+        <img class="card-img-top img-fluid mt-3" src="images/apple/<?php echo $value[5]; ?>"
+             alt="image du <?php echo "produit" . ($key + 1); ?>">
         <div class="card-body">
             <!-- product description-->
             <h4 class="card-title text-center"><?php echo $value[1]; ?></h4>
@@ -10,12 +9,13 @@
     </div>
 </div>
 <!-- Modal -->
-<div class="modal fade " id="<?php echo $key; ?>" tabindex="-1" role="dialog" aria-labelledby="<?php echo $key; ?>" aria-hidden="true">
+<div class="modal fade " id="<?php echo "produit" . ($key + 1); ?>" tabindex="-1" role="dialog"
+     aria-labelledby="<?php echo "produit" . ($key + 1); ?>" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <!-- product title -->
-                <h4 id="titredu<?php echo $key; ?>"><?php echo $value[1]; ?></h4>
+                <h4 id="titredu<?php echo "produit" . ($key + 1); ?>"><?php echo $value[1]; ?></h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -26,13 +26,14 @@
                     <div class="row mx-3">
                         <div class="col-sm-6">
                             <!-- pictures -->
-                            <img class="card-img-top img-fluid mt-3" src="images/apple/<?php echo $value[3]; ?>" alt="image du <?php echo $key; ?>">
+                            <img class="card-img-top img-fluid mt-3" src="images/apple/<?php echo $value[5]; ?>"
+                                 alt="image du <?php echo "produit" . ($key + 1); ?>">
                         </div>
                         <div class="col-sm-6">
                             <!-- prices -->
-                            <h5><i class="fab fa-bitcoin"></i><?php echo $value[0]; ?></h5>
+                            <h5><i class="fab fa-bitcoin"></i><?php echo $value[2]; ?></h5>
                             <!-- product description -->
-                            <p><?php echo $value[2]; ?></p>
+                            <p><?php echo $value[3]; ?></p>
                         </div>
                     </div>
                     <!-- product features -->
