76543f0e5ca22dd408cf7002359a7641c1e0eb6f
Vanina Diot <vanina.diot@gmail.com>
2019-03-27 14:02:57 +0100

    InsertionRaspberryDB

--- public/cardsRaspberries.php
+++ public/cardsRaspberries.php
@@ -5,25 +5,26 @@
  * Date: 2019-03-13
  * Time: 15:50
  */
-
+include "../connec.php";
+$connec = mysqli_connect(SERVER, USER, PASS, DB);
 ?>
 
 <div class="card">
-    <div data-toggle="modal" data-target="#<?php echo $key; ?>">
-        <img class="card-img-top img-fluid mt-3" src="images/raspberries/<?php echo $value[3]; ?>" alt="image du <?php echo $key; ?>">
+    <div data-toggle="modal" data-target="#<?php echo $data['id']; ?>">
+        <img class="card-img-top img-fluid mt-3" src="images/raspberries/<?php echo $data['picture']; ?>" alt="image du <?php echo $data['id']; ?>">
         <div class="card-body">
             <!-- product description-->
-            <h4 class="card-title text-center"><?php echo $value[1]; ?></h4>
+            <h4 class="card-title text-center"><?php echo $data['title']; ?></h4>
         </div>
     </div>
 </div>
 <!-- Modal -->
-<div class="modal fade " id="<?php echo $key; ?>" tabindex="-1" role="dialog" aria-labelledby="<?php echo $key; ?>" aria-hidden="true">
+<div class="modal fade " id="<?php echo $data['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="<?php echo $data['id']; ?>" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <!-- product title -->
-                <h4 id="titredu<?php echo $key; ?>"><?php echo $value[1]; ?></h4>
+                <h4 id="titredu<?php echo $data['id']; ?>"><?php echo $data['title']; ?></h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -34,13 +35,13 @@
                     <div class="row mx-3">
                         <div class="col-sm-6">
                             <!-- pictures -->
-                            <img class="card-img-top img-fluid mt-3" src="images/raspberries/<?php echo $value[3]; ?>" alt="image du <?php echo $key; ?>">
+                            <img class="card-img-top img-fluid mt-3" src="images/<?php echo $data['category']; ?>/<?php echo $data['picture']; ?>" alt="image du <?php echo $data['id']; ?>">
                         </div>
                         <div class="col-sm-6">
                             <!-- prices -->
-                            <h5><i class="fab fa-bitcoin"></i><?php echo $value[0]; ?></h5>
+                            <h5><i class="fab fa-bitcoin"></i><?php echo $data['price']; ?></h5>
                             <!-- product description -->
-                            <p><?php echo $value[2]; ?></p>
+                            <p><?php echo $data['description']; ?></p>
                         </div>
                     </div>
                     <!-- product features -->
